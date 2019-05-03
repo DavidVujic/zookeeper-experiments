0ac968b334cb5ae0a114eed714bba76163a2660d
DAEK <david.vujic@svenskaspel.se>
2019-05-03 14:26:12 +0200

    refactor: promisify setup flow

--- index.js
+++ index.js
@@ -6,15 +6,44 @@ const logger = require('./logger.js');
 const notifier = require('./notifier.js');
 
 notifier.on('connect', message => logger.log('connect', message));
-notifier.on('leader', message => logger.log('leader', message));
 notifier.on('createNode', message => logger.log('createNode', message));
-notifier.on('createWorker', message => logger.log('createWorker', message));
+
+function setupMaster() {
+  return new Promise((resolve) => {
+    notifier.on('leader', (message) => {
+      logger.log('leader', message);
+      resolve();
+    });
+
+    electLeader('/master');
+  });
+}
+
+function setupWorker() {
+  return new Promise((resolve) => {
+    notifier.on('createWorker', (message) => {
+      logger.log('createWorker', message);
+      resolve();
+    });
+
+    createWorker();
+  });
+}
+
+async function addListener(client, path) {
+  logger.log(`TODO: ${client.client_id} should listen to ${path}`);
+}
+
+async function addTask() {
+  logger.log('TODO: add task');
+}
 
 async function init() {
   await createNodes(['/workers', '/assign', '/tasks', '/status']);
-  logger.log('create nodes done');
-  electLeader('/master');
-  createWorker();
+  await setupMaster();
+  await setupWorker();
+  await addListener({ client_id: 'TODO' });
+  await addTask();
 }
 
 init().catch(logger.error);
