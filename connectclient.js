2a9511b89a04e0690783dacec019ba643b458bd8
davidvujic <david@vujic.se>
2019-05-22 15:36:49 +0200

    reconnect

--- connectclient.js
+++ connectclient.js
@@ -3,18 +3,26 @@ const ZooKeeper = require('zookeeper');
 
 function connectClient() {
   const client = new ZooKeeper();
+  let timeoutId;
 
   client.on('connect', () => {
     console.log('connect', `id=${client.client_id} state=${client.state}`);
     client.aw_exists('/', (...args) => console.log(...args), (...args) => console.log(...args));
+    clearTimeout(timeoutId);
   });
 
   client.on('connecting', () => {
     console.error('\x1b[36m', 'connecting', `id=${client.client_id} state=${client.state}`, '\x1b[0m');
+
+    timeoutId = setTimeout(() => {
+      client.close();
+      connectClient();
+    }, 5000);
   });
 
   client.on('close', () => {
     console.error('\x1b[36m', 'close', `id=${client.client_id} state=${client.state}`, '\x1b[0m');
+    clearTimeout(timeoutId);
   });
 
   client.init({
