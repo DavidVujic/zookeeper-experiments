1a0906bc08201b21dc78b42d4b60abe1952befbc
davidvujic <david@vujic.se>
2019-06-03 22:09:48 +0200

    now you can run the experiments in Docker

--- wrapper.js
+++ wrapper.js
@@ -1,9 +1,11 @@
 const ZooKeeper = require('zookeeper');
 
+const host = process.argv[2] || '127.0.0.1:2181';
+
 /** @returns {ZooKeeper} */
 function createClient(timeoutMs = 5000) {
   const config = {
-    connect: '127.0.0.1:2181',
+    connect: host,
     timeout: timeoutMs,
     debug_level: ZooKeeper.ZOO_LOG_LEVEL_WARN,
     host_order_deterministic: false,
